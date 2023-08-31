396bda61803b2dff840d3c136fab7164d1e78b94
sanjay bharathi <sanjaybharathis18087@it.ssn.edu.in>
2023-08-31 19:11:36 +0530

    Add project files.

---
+++ Core/ConfigureService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+using Topshelf_Demo;
+
+namespace WindowserviceWithTopshelf
+{
+    internal class ConfigureService
+    {
+        internal static void Configure()
+        {
+            HostFactory.Run(configure =>
+            {
+                configure.Service<MyService>(service =>
+                {
+                    service.ConstructUsing(s => new MyService());
+                    service.WhenStarted(s => s.Start());
+                    service.WhenStopped(s => s.Stop());
+                });
+                //Setup Account that window service use to run.  
+                configure.RunAsLocalSystem();
+                configure.SetServiceName("DotNetFramework");
+                configure.SetDisplayName("DotNetFramework");
+                configure.SetDescription("My .Net windows service with Topshelf");
+            });
+        }
+    }
+}
---
+++ Topshelf Demo/MyService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Topshelf_Demo
+{
+    public class MyService
+    {
+        public void Start()
+        {
+            WriteToFile("Service started. Timestamp " + DateTime.Now);
+        }
+        public void Stop()
+        {
+            WriteToFile("Service stopped. Timestamp " + DateTime.Now);
+        }
+
+        public void WriteToFile(string Message)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\LocalLog";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\LocalLog\\Log_" + DateTime.Now.Date.ToShortDateString().Replace('/', '_') + ".txt";
+            if (!File.Exists(filepath))
+            {
+                // Create a file to write to.   
+                using (StreamWriter sw = File.CreateText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+        }
+    }
+}
